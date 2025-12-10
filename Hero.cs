b99bb4671ed2a7b8afdad1a6ce13383c5341bf0a
jonathandenies <s151090@ap.be>
2025-12-10 14:43:11 +0100

    Add base for movement (character moves and is restricted to the screen)

--- Hero.cs
+++ Hero.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using SoftwareEngineeringProject.Interfaces;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 
 namespace SoftwareEngineeringProject
@@ -9,8 +11,12 @@ namespace SoftwareEngineeringProject
     {
         private Texture2D texture;
         Animation animation;
-
+        
         private int schuifOp_X = 0;
+
+        private Vector2 positie = new Vector2(0, 0);
+        private Vector2 snelheid = new Vector2(2,2);
+        
 
         public Hero(Texture2D texture)
         {
@@ -24,12 +30,30 @@ namespace SoftwareEngineeringProject
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, new Vector2(0, 0), animation.CurrentFrame.SourceRectangle, Color.White);
+            spriteBatch.Draw(texture, positie, animation.CurrentFrame.SourceRectangle, Color.White);
         }
 
         public void Update(GameTime gameTime)
         {
             animation.Update(gameTime);
+            Move();
+        }
+
+        private void Move()
+        {
+            positie += snelheid;
+            if (positie.X > 768
+                || positie.X < 0)
+            {
+                snelheid.X *= -1;
+            }
+            if (positie.Y > 448
+                || positie.Y < 0)
+            {
+                snelheid.Y *= -1;
+            }
+
+
         }
     }
 }
